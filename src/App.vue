5e60045e9385e13bbc9b6ba75db8379d32ff32cb
Brunna Serafina <brunnaserafina@gmail.com>
2025-02-25 13:36:56 -0300

    feat: Add page title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<h1>Buscador de CEP</h1>
+  <div class="page-title">
+    <span class="material-symbols-outlined"> explore </span>
+    <h1>Buscador de CEP</h1>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.page-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  background-color: #ffd57a;
+  padding: 1rem;
+  border-radius: 0px 0.5rem 0.5rem 0px;
+  position: fixed;
+  top: 2rem;
+  left: 0;
+  text-transform: uppercase;
 
+  h1 {
+    font-family: "Barlow", serif;
+    font-weight: 500;
+  }
+
+  h1,
+  span {
+    font-size: 2.5rem;
+  }
+}
 </style>
